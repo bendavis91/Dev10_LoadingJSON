81a4f583164b64a4411ec8efdf66e5fd0cce12e8
bendavis91 <60115174+bendavis91@users.noreply.github.com>
2020-04-21 09:55:12 -0500

    Add files via upload

--- js/data-json2.js
+++ js/data-json2.js
@@ -13,7 +13,7 @@ xhr.onload = function() {                       // When readystate changes
     var newContent = '';
     for (var i = 0; i < responseObject.events.length; i++) { // Loop through object
       newContent += '<div class="event">';
-      newContent += '<img src="' + responseObject.events[i].map + '" ';
+      newContent += '<img src="' + responseObject.events[i].map + '" width = 250 height = 200 ';
       newContent += 'alt="' + responseObject.events[i].location + '" />';
       newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
       newContent += responseObject.events[i].date + '</p>';
@@ -21,12 +21,12 @@ xhr.onload = function() {                       // When readystate changes
     }
 
     // Update the page with the new content
-    document.getElementById('content').innerHTML = newContent;
+    document.getElementById('contentTwo').innerHTML = newContent;
 
   //}
 };
 
-xhr.open('GET', 'data/data.json', true);        // Prepare the request
+xhr.open('GET', 'data/data2.json', true);        // Prepare the request
 xhr.send(null);                                 // Send the request
 
 
